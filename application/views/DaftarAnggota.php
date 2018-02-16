dc3125f452837ea9986e1923c6fa3e59f38f4198
Laatansa <laatansaimroni@gmail.com>
2018-02-17 00:16:13 +0700

    Revert "Fixing modal on DaftarAnggota"
    This reverts commit 6e456c788a574e9a2ce1d7e3871323266a4617b0.

--- application/views/DaftarAnggota.php
+++ application/views/DaftarAnggota.php
@@ -5,7 +5,7 @@
 		<div class="container-fluid">
 			<div class="row">
 				<div class="col-md-12">
-					<h1 class="page-header">Daftar Anggota</h1>
+					<h1 class="page-header">Kelola Anggota</h1>
 				</div>
 			</div>
 			<?php 
@@ -13,8 +13,9 @@
 					echo $this->session->flashdata('message');
 				}
 			?>
-			</div>
+			
 			<div class="row">
+			<div class="col-md-12">
 					<form>
 					<table width="100%" class="table table-striped table-bordered table-hover" id="dataTables-example">
 						<thead>
@@ -38,27 +39,40 @@
 								<td><?php echo $row->alamat;?></td>
 								<td>
 									<a href="<?php echo base_url('Anggota/dataAnggota/'.$row->no_induk);?>"><button type="button" class="btn btn-primary">Detail</button></a>
-									<?php 
-									if($this->session->userdata('level')=='admin'){?>
+									
 									<!-- Button trigger modal -->
-									<button type="button" class="btn btn-danger" data-toggle="modal" data-target="#deleteModal" data-no-induk="<?php echo $row->no_induk; ?>" data-nama="<?php echo $row->nama; ?>" data-url="<?php echo current_url();?>">Hapus</button>
+									<button type="button" class="btn btn-danger" data-toggle="modal" data-target="#hapusModal" data-no-induk="<?php echo $row->no_induk; ?>" data-nama="<?php echo $row->nama; ?>" data-url="<?php echo current_url();?>">Hapus</button>
 									<button type="button" class="btn btn-warning" data-toggle="modal" data-target="#resetModal" data-no-induk="<?php echo $row->no_induk; ?>" data-nama="<?php echo $row->nama; ?>" data-url="<?php echo current_url();?>">Reset Password</button>
-									<?php }?>
+									
 								</td>
 							</tr>
 						<?php 
 							}
 						}
 						?>
+						
 						</tbody>
+						<tfoot>
+					<tr>
+					<td>
+					<a href="<?php echo base_url('Anggota/tambahAnggota');?>"><button type="button" class="btn btn-primary"><i class="fa fa-plus "></i> Tambah Anggota</button></a>
+					</td>
+					<td></td>
+					<td></td>
+					<td></td>
+					</tr>
+					</tfoot>
 					</table>
+					
 					</form>
+					</div>
+			</div>
 			</div>
 		</div>
-	</div>
+	
 </body>
 <!-- Modal -->
-<div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+<div class="modal fade" id="hapusModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
@@ -70,7 +84,7 @@
       Semua data yang berhubungan dengan akun yang bersangkutan juga akan dihapus 
       </div>
       <div class="modal-footer">
-      <form action="<?php echo base_url('Anggota/deleteAnggota'); ?>" method="post">
+      <form action="<?php echo base_url('Anggota/hapusAnggota'); ?>" method="post">
       	<input type="hidden" class="no-induk" name="no-induk"/>
       	<input type="hidden" class="url" name="url"/>
         <button type="button" class="btn btn-default" data-dismiss="modal">Batal</button>
@@ -91,7 +105,7 @@
       </div>
       <div class="modal-body">
       Apakah anda yakin ingin mereset password ?
-      Password yang direset akan disamakan dengan NIP. Segera ganti password untuk keamanan akun!  
+      Password yang direset akan disamakan dengan no induk. Segera ganti password untuk keamanan akun!  
       </div>
       <div class="modal-footer">
       <form action="<?php echo base_url('Akun/resetPassword'); ?>" method="post">
@@ -117,32 +131,32 @@ $(document).ready( function () {
 	$('#dataTables-example').DataTable({
 		"lengthChange": false
 	});
-	$('#deleteModal').on('show.bs.modal', function (event) {
+	 $('#hapusModal').on('show.bs.modal', function (event) {
 		  var button = $(event.relatedTarget) // Button that triggered the modal
-		  var no_induk = button.data('no-induk') // Extract info from data-* attributes
+		  var no-induk = button.data('no-induk') // Extract info from data-* attributes
 		  var nama = button.data('nama')
 		  var url = button.data('url') 
 		  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
 		  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
 		  var modal = $(this)
 		  modal.find('.nama').text(nama)
-		  modal.find('.no-induk').text(no_induk)
-		  modal.find('input','.nip').val(no_induk)
+		  modal.find('.no-induk').text(no-induk)
+		  modal.find('input','.induk').val(induk)
 		  modal.find('.url').val(url)
 		})
 	$('#resetModal').on('show.bs.modal', function (event) {
 		  var button = $(event.relatedTarget) // Button that triggered the modal
-		  var no_induk = button.data('no-induk') // Extract info from data-* attributes
+		  var no-induk = button.data('no-induk') // Extract info from data-* attributes
 		  var nama = button.data('nama')
 		  var url = button.data('url') 
 		  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
 		  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
 		  var modal = $(this)
 		  modal.find('.nama').text(nama)
-		  modal.find('.no-induk').text(no_induk)
-		  modal.find('input','.nip').val(no_induk)
+		  modal.find('.no-induk').text(no-induk)
+		  modal.find('input','.no-induk').val(no-induk)
 		  modal.find('.url').val(url)
-		})
+		}) 
 });
 </script>
 
